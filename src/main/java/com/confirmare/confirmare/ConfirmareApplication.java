5b31cf91601e66f56aec3f9ce27bd0fcf3964112
Paulo Pereira <paulo.pereira@liferay.com>
2026-04-29 20:27:39 -0300

    Initial gradle and spring setup

---
+++ src/main/java/com/confirmare/confirmare/ConfirmareApplication.java
@@ -0,0 +1,13 @@
+package com.confirmare.confirmare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfirmareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/confirmare/confirmare/ConfirmareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confirmare.confirmare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
